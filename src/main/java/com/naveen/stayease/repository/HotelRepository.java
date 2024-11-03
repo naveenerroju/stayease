13a4119db23dd3e7bbddceda1d0e464e7c21f70c
Naveen Kumar <naveenkumarerroju@gmail.com>
2024-11-03 12:52:09 +0530

    removing unused method

--- src/main/java/com/naveen/stayease/repository/HotelRepository.java
+++ src/main/java/com/naveen/stayease/repository/HotelRepository.java
@@ -4,10 +4,7 @@ import com.naveen.stayease.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
-    List<Hotel> findByAvailability(boolean availability);
 }
